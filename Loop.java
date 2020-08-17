0444c1ef6b9cd60b2f9403ade33a76d3c7b34aeb
Bonoco <ernestogarcia.csula@gmail.com>
2020-08-17 16:32:48 -0700

    Moved files into the folders

---
+++ Loop.java
@@ -0,0 +1,17 @@
+
+public class Loop {
+
+	public static void main(String[] args) {
+		
+		int num = 10;
+		
+		for(int i = 0; i < 10; i++) {
+			
+			System.out.println("Number: " + num);
+			num+= 10;
+			
+		}
+
+	}
+
+}
